61ba91cd8e35b55e3abe7511dc15c8888b7bb891
Kiri Kiri <killian.podhajsky@gmail.com>
2023-03-31 13:49:01 -0700

    Psuedo Code

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+
+// TODO: create var to link back to document.querySelector
+
+//TODO: create an array of possible words/ Name the array
+
+//TODO: Randomize possible answers
+
+//TODO: log user guesses
+	// Correct guesses to populate in answer
+	//incorrect guesses logged
+
+//TODO: Include Countdown
+
+//TODO: 
+// Correct Answers-- You Win~!
+// Incorrect Answers -- You Lose! Game Over~!
+   // Include final score
+
+//TODO: Option to play again
